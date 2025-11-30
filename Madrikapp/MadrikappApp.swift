745009f5598b9455ed2234d95202dee1bbdb9b98
wasan <twasanj@gmail.com>
2025-11-30 11:45:20 +0300

    Initial Commit

---
+++ Madrikapp/MadrikappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadrikappApp.swift
+//  Madrikapp
+//
+//  Created by wasan jayid althagafi on 09/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MadrikappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
